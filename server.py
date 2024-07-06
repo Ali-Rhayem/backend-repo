6dd176b0410982d5eab91f8a90bbe38c7f53a589
Alirh1 <alirhayem.1212@gmail.com>
2024-07-06 22:43:10 +0300

    Added Server class with startup logic

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
 import yaml
+
+class Server:
+    def __init__(self, config):
+        self.config = config
+
+    def start(self):
+        if self.config['ServerIPAddress'] == '127.0.0.1':
+            print("Server is running on localhost")
+
+if __name__ == "__main__":
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    server = Server(config)
+    server.start()
